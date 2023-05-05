f245814bbdcbb9460466e64495da558fca47e5ff
Vinícius Souza <tradutorvinicius@gmail.com>
2023-05-05 17:07:42 -0300

    Resolve requisitos 2, 3 e 4

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import Provider from './contexts/MyProvider';
 
 ReactDOM
   .createRoot(document.getElementById('root'))
   .render(
-    <Provider>
-      <App />
-    </Provider>,
+    <BrowserRouter>
+      <Provider>
+        <App />
+      </Provider>
+    </BrowserRouter>,
   );
---
+++ src/tests/Login.test.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../helpers/renderWithRouter';
+
+const testIdEmail = 'email-input';
+const testIdPassword = 'password-input';
+
+describe('Testa o componente Login', () => {
+  test('Verifica se os inputs são renderizados na tela', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const { pathname } = history.location;
+
+    expect(pathname).toBe('/');
+
+    const inputEmail = screen.getByTestId(testIdEmail);
+    const inputPassword = screen.getByTestId(testIdPassword);
+
+    expect(inputEmail).toBeInTheDocument();
+    expect(inputPassword).toBeInTheDocument();
+  });
+
+  test('Verifica se o botão está desabilitado ao digitar informações inválidas', () => {
+    renderWithRouter(<App />);
+
+    const inputEmail = screen.getByTestId(testIdEmail);
+    const inputPassword = screen.getByTestId(testIdPassword);
+    const btnEnter = screen.getByTestId('login-submit-btn');
+
+    userEvent.type(inputEmail, 'xablau');
+    userEvent.type(inputPassword, '123456');
+
+    expect(btnEnter).toBeDisabled();
+  });
+
+  test('Verifica se o botão está habilitado ao digitar informações válidas', () => {
+    renderWithRouter(<App />);
+
+    const inputEmail = screen.getByTestId(testIdEmail);
+    const inputPassword = screen.getByTestId(testIdPassword);
+    const btnEnter = screen.getByTestId('login-submit-btn');
+
+    userEvent.type(inputEmail, 'xablau@gmail.com');
+    userEvent.type(inputPassword, '123456');
+
+    expect(btnEnter).toBeEnabled();
+  });
+});
