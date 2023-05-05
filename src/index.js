caa35a9d82b64db0c3a5a62a96bdb2902fc1aff8
Vinícius Souza <tradutorvinicius@gmail.com>
2023-05-05 15:46:15 -0300

    feat: setup inicial

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Provider from './contexts/MyProvider';
 
 ReactDOM
   .createRoot(document.getElementById('root'))
-  .render(<App />);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  .render(
+    <Provider>
+      <App />
+    </Provider>,
+  );
