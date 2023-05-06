5cbe19d69a358cc6542577a0db2fbf1ebc245854
Luar <luarrezende@gmail.com>
2023-05-05 22:22:42 -0300

    fix: router blank line

--- src/Routes/router.js
+++ src/Routes/router.js
@@ -6,7 +6,6 @@ import Profile from '../pages/Profile';
 import DoneRecipes from '../pages/DoneRecipes';
 import FavoriteRecipes from '../pages/FavoriteRecipes';
 
-
 function Router() {
   return (
     <Switch>
