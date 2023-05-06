34bc99fab7d3fad66baaa28ac253546da18e2059
Luar <luarrezende@gmail.com>
2023-05-06 02:10:31 -0300

    feat: resolve requisitos do 57 ao 62

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,10 +1,54 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 
 function Profile() {
+  const history = useHistory();
+  const emailStorage = JSON.parse(localStorage.getItem('user'));
+  const verifyEmail = () => {
+    if (emailStorage && emailStorage.email) {
+      const { email } = emailStorage;
+      return email;
+    }
+  };
+
+  const doneRecipesPush = () => {
+    history.push('/done-recipes');
+  };
+
+  const favoriteRecipesPush = () => {
+    history.push('/favorite-recipes');
+  };
+
+  const logout = () => {
+    localStorage.clear();
+    history.push('/');
+  };
   return (
     <div>
       <Header title="Profile" searchIcon={ false } />
+      <p data-testid="profile-email">{ verifyEmail() }</p>
+      <button
+        type="button"
+        data-testid="profile-done-btn"
+        onClick={ doneRecipesPush }
+      >
+        Done Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+        onClick={ favoriteRecipesPush }
+      >
+        Favorite Recipes
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+        onClick={ logout }
+      >
+        Logout
+      </button>
     </div>
   );
 }
--- src/tests/Profile.test.js
+++ src/tests/Profile.test.js
@@ -1,7 +1,66 @@
 import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../helpers/renderWithRouter';
 import Profile from '../pages/Profile';
 
 describe('Testa página Profile', () => {
-  test('', () => { renderWithRouter(<Profile />); });
+  const login = {
+    email: 'john.doe@example.com',
+  };
+
+  test('testa se botão redireciona para rota "/done-recipes"', () => {
+    const { history } = renderWithRouter(<Profile />);
+
+    localStorage.setItem('user', JSON.stringify(login));
+
+    const btn = screen.getByRole('button', {
+      name: /done recipes/i,
+    });
+
+    userEvent.click(btn);
+
+    const { pathname } = history.location;
+
+    expect(pathname).toBe('/done-recipes');
+  });
+  test('testa se botão redireciona para rota "/favorite-recipes"', () => {
+    const { history } = renderWithRouter(<Profile />);
+
+    localStorage.setItem('user', JSON.stringify(login));
+
+    const btn = screen.getByRole('button', {
+      name: /favorite recipes/i,
+    });
+
+    userEvent.click(btn);
+
+    const { pathname } = history.location;
+
+    expect(pathname).toBe('/favorite-recipes');
+  });
+  test('testa se botão redireciona para rota "/"', () => {
+    const { history } = renderWithRouter(<Profile />);
+
+    localStorage.setItem('user', JSON.stringify(login));
+
+    const btn = screen.getByRole('button', {
+      name: /Logout/i,
+    });
+
+    userEvent.click(btn);
+
+    const { pathname } = history.location;
+
+    expect(pathname).toBe('/');
+  });
+  test('testa se email aparece no perfil', () => {
+    renderWithRouter(<Profile />);
+
+    localStorage.setItem('user', JSON.stringify(login));
+
+    const email = JSON.parse(localStorage.getItem('user'));
+
+    expect(email).toEqual(login);
+  });
 });
