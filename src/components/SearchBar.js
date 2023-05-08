92a497edb134f48d75167f8527ab9f8deab4b11c
Luar <luarrezende@gmail.com>
2023-05-08 16:31:03 -0300

    feat: requisito 11

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,106 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import context from '../contexts/MyContext';
+import { mealsIngredients, mealsNames, mealsFirstLetter,
+  drinkIngredients, drinkNames, drinkFirstLetter } from '../service/APIs';
 
 function SearchBar() {
+  const { inputApi, setFilterData } = useContext(context);
+  const { pathname } = useLocation();
+  const [radio, setRadio] = useState('');
+
+  const handleChange = ({ target: { value } }) => {
+    setRadio(value);
+  };
+
+  const getMealsApi = async () => {
+    if (radio === 'ingredient') {
+      const filterIngredients = await mealsIngredients(inputApi);
+      setFilterData(filterIngredients);
+    }
+    if (radio === 'name') {
+      const filterName = await mealsNames(inputApi);
+      setFilterData(filterName);
+    }
+    if (radio === 'first-letter') {
+      if (inputApi.length === 1) {
+        const filterLetter = await mealsFirstLetter(inputApi);
+        setFilterData(filterLetter);
+      } else {
+        global.alert('Your search must have only 1 (one) character');
+      }
+    }
+  };
+
+  const getDrinksApi = async () => {
+    if (radio === 'ingredient') {
+      const filterIngredients = await drinkIngredients(inputApi);
+      setFilterData(filterIngredients);
+    }
+    if (radio === 'name') {
+      const filterName = await drinkNames(inputApi);
+      setFilterData(filterName);
+    }
+    if (radio === 'first-letter') {
+      if (inputApi.length === 1) {
+        const filterLetter = await drinkFirstLetter(inputApi);
+        setFilterData(filterLetter);
+      } else {
+        global.alert('Your search must have only 1 (one) character');
+      }
+    }
+  };
+
+  const handleClick = () => {
+    setFilterData([]);
+    if (pathname === '/meals') {
+      getMealsApi();
+    }
+    if (pathname === '/drinks') {
+      getDrinksApi();
+    }
+  };
+
   return (
     <nav>
-      <input type="radio" data-testid="ingredient-search-radio" />
-      <input type="radio" data-testid="name-search-radio" />
-      <input type="radio" data-testid="first-letter-search-radio" />
-      <button data-testid="exec-search-btn">Search</button>
+      <div
+        onChange={ handleChange }
+      >
+        <label>
+          <input
+            name="filter"
+            type="radio"
+            data-testid="ingredient-search-radio"
+            value="ingredient"
+          />
+          Ingredient
+        </label>
+        <label>
+          <input
+            name="filter"
+            type="radio"
+            data-testid="name-search-radio"
+            value="name"
+          />
+          Name
+        </label>
+        <label>
+          <input
+            name="filter"
+            type="radio"
+            data-testid="first-letter-search-radio"
+            value="first-letter"
+          />
+          First letter
+        </label>
+      </div>
+      <button
+        data-testid="exec-search-btn"
+        onClick={ handleClick }
+      >
+        Search
+
+      </button>
     </nav>
   );
 }
--- src/contexts/MyProvider.js
+++ src/contexts/MyProvider.js
@@ -2,16 +2,25 @@ import React, { useState, useMemo } from 'react';
 import PropTypes from 'prop-types';
 import MyContext from './MyContext';
 
-const INITIAL_STATE1 = {};
-const INITIAL_STATE2 = {};
-
 function Provider({ children }) {
-  const [state1, setState1] = useState(INITIAL_STATE1);
-  const [state2, setState2] = useState(INITIAL_STATE2);
+  const [inputApi, setInputApi] = useState('');
+  const [data, setData] = useState([]);
+  const [filterData, setFilterData] = useState([]);
 
   const values = useMemo(() => ({
-    state1, state2, setState1, setState2,
-  }), [state1, state2, setState1, setState2]);
+    inputApi,
+    setInputApi,
+    data,
+    setData,
+    filterData,
+    setFilterData,
+  }), [
+    inputApi,
+    setInputApi,
+    data,
+    setData,
+    filterData,
+    setFilterData]);
 
   return (
     <MyContext.Provider value={ values }>
